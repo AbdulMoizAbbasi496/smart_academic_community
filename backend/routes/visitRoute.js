97b07b5f11d425d479ea2e1fe4827e8f4ee51290
Abdul Moiz Abbasi <moiz45573@gmail.com>
2026-06-01 16:18:37 +0500

    Update visitRoute.js

--- backend/routes/visitRoute.js
+++ backend/routes/visitRoute.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Visit = require("./models/Visit");
+const Visit = require("../models/Visit");
 router.get("/api/visits", async (req, res) => {
   try {
     const visit = await Visit.findOneAndUpdate(
